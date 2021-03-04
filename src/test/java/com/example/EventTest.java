f337ef7b2372bfd9be4390563b2bc2012c1bf2af
zirpin <zirpin@gmail.com>
2021-03-04 11:32:14 +0100

    test add and set attendees

--- src/test/java/com/example/EventTest.java
+++ src/test/java/com/example/EventTest.java
@@ -3,13 +3,15 @@ package com.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
     @Test
     @DisplayName("Test add attendee")
-    public void addAttendee()
+    public void TestAddAttendee()
     {
         Attendee attendee1 = new Attendee(1L,"thor","thor@email.es");
         System.out.println(attendee1.getId()+" "+attendee1.getNickname()+" "+ attendee1.getEmail());
@@ -17,6 +19,26 @@ class EventTest {
         eventTester.addAttendee(attendee1);;
         assertNotNull(attendee1);
         assertNotNull(eventTester);
+    }
+
+    @Test
+    @DisplayName("Test add and set attendees")
+    public void testAddAttendees()
+    {
+        Event eventTester = new Event();
+        ArrayList<Attendee> attendeeList = new ArrayList<>();
+        Attendee attendee1 = new Attendee(1L,"thor","thor@email.es");
+        Attendee attendee2 = new Attendee(2L,"loki","loki@email.es");
+
+        attendeeList.add(0,attendee1);
+        attendeeList.add(1,attendee2);
+
+        eventTester.addAttendees(attendeeList);
+        eventTester.setAttendees(attendeeList);
+
+        assertNotNull(eventTester.getAttendees());
+        assertEquals(attendeeList.stream().count(),2);
+
     }
 
 }
