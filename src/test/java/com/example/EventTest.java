2ef4818e6591b3e2245ad7e9a765ff896a912a2c
zirpin <zirpin@gmail.com>
2021-03-04 12:57:50 +0100

    Test remove attendee object and code update

--- src/test/java/com/example/EventTest.java
+++ src/test/java/com/example/EventTest.java
@@ -26,12 +26,15 @@ class EventTest {
     @DisplayName("Test add attendee")
     public void TestAddAttendee()
     {
-        Attendee attendee1 = new Attendee(1L,"thor","thor@email.es");
-        System.out.println(attendee1.getId()+" "+attendee1.getNickname()+" "+ attendee1.getEmail());
+        Attendee attendee1 = new Attendee();
+        attendee1.setId(1L);
+        attendee1.setNickname("thor");
+        attendee1.setEmail("thor@email.es");
+
         Event eventTester = new Event();
         eventTester.addAttendee(attendee1);;
         assertNotNull(attendee1);
-        assertNotNull(eventTester);
+        assertEquals(eventTester.getAttendees().size(),1);
     }
 
     @Test
@@ -89,4 +92,28 @@ class EventTest {
         assertEquals(attendeeList.stream().count(), 0);
     }
 
+    @Test
+    @DisplayName("Test create speaker")
+    public void createSpeaker()
+    {
+        Speaker speakerSenior = new Speaker(1L,"John","senior");
+        Speaker speakerNull = new Speaker();
+        Event eventTester = new Event();
+        eventTester.addSpeaker(speakerSenior);
+        assertNotNull(eventTester);
+        assertEquals(eventTester.getSpeakers().size(),1);
+        }
+
+    @Test
+    @DisplayName("Test remove speaker")
+    public void removeSpeaker()
+    {
+        Speaker speakerNormal = new Speaker();
+        speakerNormal.setId(2L);
+        speakerNormal.setName("Jim");
+        speakerNormal.setExpertise("normal");
+        Event eventTester = new Event();
+        eventTester.removeSpeaker(speakerNormal);
+        assertEquals(eventTester.getSpeakers().size(),0);
+    }
 }
